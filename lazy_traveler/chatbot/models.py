d5f7dada85e352fb1c0a929761dfb1d298674c52
Marvin Baek <60081679+MarvinBaek@users.noreply.github.com>
2025-03-15 01:22:57 +0900

    models.py의 ChatHistory User 모델과 연동으로 수정

--- lazy_traveler/chatbot/models.py
+++ lazy_traveler/chatbot/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-
+from accounts.models import User  # User 모델 import
 
 class ChatHistory(models.Model):
-    username = models.CharField(max_length=255)  # 사용자 이름
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # User 모델과 연동
     message = models.TextField()  # 사용자의 메시지
     response = models.TextField()  # 챗봇의 응답
     created_at = models.DateTimeField(auto_now_add=True)  # 대화 시간
     session_id = models.CharField(max_length=255)  # 세션 ID
 
     def __str__(self):
-        return f"{self.username} - {self.message[:20]}"
+        return f"{self.user.username} - {self.message[:20]}"
