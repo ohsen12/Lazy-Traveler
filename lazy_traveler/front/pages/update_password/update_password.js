ef232d72ba268df2d8f449096f2f6b9ee5217aba
seeun Oh <tpsdms121@gmail.com>
2025-03-15 14:01:13 +0900

    update update_password.js

--- lazy_traveler/front/pages/update_password/update_password.js
+++ lazy_traveler/front/pages/update_password/update_password.js
@@ -8,9 +8,20 @@ const updatePassword = async () => {
         return;
     }
 
+    // 엑세스 토큰 가져오기
+    const accessToken = localStorage.getItem("access_token");
+    if (!accessToken) {
+        alert("로그인이 필요합니다.");
+        return;
+    }
+
     try {
         const response = await axios.post("http://127.0.0.1:8000/accounts/update_password/", {
             new_password: newPassword
+        }, {
+            headers: {
+                'Authorization': `Bearer ${accessToken}`  // 엑세스 토큰을 Authorization 헤더에 추가
+            }
         });
 
         console.log("비밀번호 변경 성공:", response.data);
