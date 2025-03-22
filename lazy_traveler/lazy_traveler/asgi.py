a05d6316ca1ecba53e1774b876610f3d8d716fcb
Marvin Baek <60081679+MarvinBaek@users.noreply.github.com>
2025-03-22 20:18:44 +0900

    장고 앱 초기화 이후 urlpatters import

--- lazy_traveler/lazy_traveler/asgi.py
+++ lazy_traveler/lazy_traveler/asgi.py
@@ -1,13 +1,20 @@
 import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lazy_traveler.settings")  # noqa: E402
+django.setup()  # noqa: E402
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from chatbot.routing import websocket_urlpatterns
+
 from chatbot.middleware import JWTAuthMiddleware  # noqa: E402
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lazy_traveler.settings')
+django_asgi_app = get_asgi_application()
+
+from chatbot.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    'http': django_asgi_app,
     'websocket': JWTAuthMiddleware(
         inner=URLRouter(
             websocket_urlpatterns
