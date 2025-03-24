26ef2b472d5857877a769cd4ccd46a6f696ef319
seeun Oh <tpsdms121@gmail.com>
2025-03-24 15:13:31 +0900

    mypage.js 에서 메인페이지로 이동 경로 수정(map->main)

--- lazy_traveler/front/pages/mypage/mypage.js
+++ lazy_traveler/front/pages/mypage/mypage.js
@@ -1,6 +1,6 @@
-// 맵 페이지로 이동
+// 메인 페이지로 이동
 function goToMap() {
-    window.location.href = "https://lazy-traveler.store/pages/map/map.html";
+    window.location.href = "https://lazy-traveler.store/pages/main/main.html";
 }
 
 document.addEventListener("DOMContentLoaded", async function() {
