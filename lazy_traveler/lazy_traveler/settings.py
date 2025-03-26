a0feb327e9504f4b54397257a042195444d3b743
seeun Oh <tpsdms121@gmail.com>
2025-03-26 21:49:24 +0900

    static 로직 제거(사용하지 않음)

--- lazy_traveler/lazy_traveler/settings.py
+++ lazy_traveler/lazy_traveler/settings.py
@@ -183,11 +183,6 @@ USE_I18N = True
 USE_TZ = False # ✅ 서버 시간대
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
